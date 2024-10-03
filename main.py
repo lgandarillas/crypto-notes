a05b847ded7019c0e258e8eeb936190d48c96a02
lgandarillas <lgandaf@gmail.com>
2024-10-03 23:14:23 +0200

    Refactor code to mode_handler.py

--- main.py
+++ main.py
@@ -5,38 +5,21 @@ It will be the entry point for the application.
 By: Luis Gandarillas && Carlos Bravo
 """
 
-import readline
 from prints import PrintManager
-
-def setup_readline_history():
-    """Add basic commands to the readline history."""
-    for cmd in ["register", "login", "exit"]:
-        readline.add_history(cmd)
-
-def handle_mode(mode: str, printer: PrintManager) -> None:
-    """Handle the user's selected mode."""
-    if mode == "register":
-        print(f"You selected {printer.COLOR_BLUE}register{printer.COLOR_RESET}\n")
-    elif mode == "login":
-        print(f"You selected {printer.COLOR_BLUE}login{printer.COLOR_RESET}\n")
-    elif mode == "exit":
-        printer.print_exit_msg()
-        return False
-    else:
-        print(f"Invalid mode: {printer.COLOR_RED}{mode}{printer.COLOR_RESET}\n")
-    return True
+from mode_handler import ModeHandler
 
 def main():
     printer = PrintManager()
     printer.print_welcome_msg()
 
-    setup_readline_history()
+    mode_handler = ModeHandler(printer)
+    mode_handler.setup_readline_history()
 
     while True:
         try:
             # Get the users mode
             mode = input(f"Select a mode ({printer.COLOR_BLUE}register{printer.COLOR_RESET}, {printer.COLOR_BLUE}login{printer.COLOR_RESET}, {printer.COLOR_BLUE}exit{printer.COLOR_RESET}): ").strip().lower()
-            if not handle_mode(mode, printer):
+            if not mode_handler.handle_mode(mode):
                 break
         except KeyboardInterrupt:
             # Handle Ctrl+C by showing the exit message
---
+++ mode_handler.py
@@ -0,0 +1,45 @@
+"""
+This file contains the mode handler for the program.
+"""
+
+import readline
+
+class ModeHandler:
+	MODES = {
+		"register": "register",
+		"login": "login",
+		"exit": "exit"
+	}
+
+	def __init__(self, printer):
+		self.printer = printer
+		self.mode_handlers = {
+			self.MODES["register"]: self.handle_register,
+			self.MODES["login"]: self.handle_login,
+			self.MODES["exit"]: self.handle_exit
+		}
+
+	def setup_readline_history(self):
+		"""Add basic commands to the readline history."""
+		for cmd in self.MODES.values():
+			readline.add_history(cmd)
+
+	def handle_mode(self, mode: str) -> bool:
+		handler = self.mode_handlers.get(mode, lambda: self.handle_invalid_mode(mode))
+		return handler()
+
+	def handle_register(self):
+		print(f"You selected {self.printer.COLOR_BLUE}register{self.printer.COLOR_RESET}\n")
+		return True
+
+	def handle_login(self):
+		print(f"You selected {self.printer.COLOR_BLUE}login{self.printer.COLOR_RESET}\n")
+		return True
+
+	def handle_exit(self):
+		self.printer.print_exit_msg()
+		return False
+
+	def handle_invalid_mode(self, mode: str):
+		print(f"Invalid mode: {self.printer.COLOR_RED}{mode}{self.printer.COLOR_RESET}\n")
+		return True
