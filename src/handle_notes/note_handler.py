3e6cd2052cf789db9ce3bf8811cbf5419eeea4b3
lgandari <lgandaf@gmail.com>
2024-11-28 18:00:56 +0100

    Update note handler with aux functions.

--- src/handle_notes/note_handler.py
+++ src/handle_notes/note_handler.py
@@ -33,7 +33,7 @@ class NoteHandler:
 			try:
 				mode = input(f"\nSelect a mode ({self.printer.COLOR_BLUE}new{self.printer.COLOR_RESET}, {self.printer.COLOR_BLUE}read{self.printer.COLOR_RESET}, {self.printer.COLOR_BLUE}list{self.printer.COLOR_RESET}, {self.printer.COLOR_BLUE}delete{self.printer.COLOR_RESET}, {self.printer.COLOR_BLUE}exit{self.printer.COLOR_RESET}) for user {self.username}: ").strip().lower()
 
-				if mode == "exit":
+				if not mode or mode == "exit":
 					self.printer.print_action(f"Exiting notes app for user {self.username}")
 					break
 				else:
@@ -85,29 +85,38 @@ class NoteHandler:
 		note_actions.create(note_name, note_content)
 		self._save_notes(notes)
 
+	@staticmethod
+	def _ensure_directory_exists(directory_path):
+		"""Ensures that the specified directory exists, creating it if necessary."""
+		if not os.path.exists(directory_path):
+			os.makedirs(directory_path)
+
+	def _decrypt_notes_file(self):
+		"""Decrypts and loads the notes file, returning the decrypted notes as a string."""
+		with open(self.notes_file, 'rb') as file:
+				encrypted_data = json.load(file)
+
+		encrypted_session_key = bytes.fromhex(encrypted_data["encrypted_session_key"])
+		nonce = bytes.fromhex(encrypted_data["nonce"])
+		ciphertext = bytes.fromhex(encrypted_data["ciphertext"])
+		aad = bytes.fromhex(encrypted_data["aad"])
+
+		session_key = decrypt_session_key(self.rsa_private_key, encrypted_session_key)
+		notes_data_str = decrypt_notes_data(nonce, session_key, aad, ciphertext, self.rsa_public_key)
+
+		return notes_data_str
+
 	def _load_notes(self):
 		"""Loads notes from a file associated with the user."""
-		if not os.path.exists(self.notes_dir):
-			os.makedirs(self.notes_dir)
+		self._ensure_directory_exists(self.notes_dir)
 
 		if not os.path.exists(self.notes_file):
 			return []
 
 		try:
-			with open(self.notes_file, 'rb') as file:
-				encrypted_data = json.load(file)
-
-			encrypted_session_key = bytes.fromhex(encrypted_data["encrypted_session_key"])
-			nonce = bytes.fromhex(encrypted_data["nonce"])
-			ciphertext = bytes.fromhex(encrypted_data["ciphertext"])
-			aad = bytes.fromhex(encrypted_data["aad"])
-
-			session_key = decrypt_session_key(self.rsa_private_key, encrypted_session_key)
-			notes_data_str = decrypt_notes_data(nonce, session_key, aad, ciphertext, self.rsa_public_key)
-
+			notes_data_str = self._decrypt_notes_file()
 			self.printer.print_debug("[CRYPTO LOG] Notes successfully decrypted and loaded.")
 			return json.loads(notes_data_str)
-
 		except ValueError as e:
 			self.printer.print_error(f"[SECURITY ALERT] {e}")
 			exit(1)
@@ -115,25 +124,31 @@ class NoteHandler:
 			self.printer.print_error(f"[SECURITY ALERT] Failed to load notes: {e}")
 			exit(1)
 
+	def _encrypt_notes_file(self, notes):
+		"""Encrypts and prepares notes data for saving."""
+		session_key = generate_session_key()
+		notes_data = json.dumps(notes)
+
+		nonce, ciphertext, aad = encrypt_notes_data(notes_data, session_key, self.rsa_private_key)
+		encrypted_session_key = encrypt_session_key(self.rsa_public_key, session_key)
+
+		encrypted_data = {
+			"nonce": nonce.hex(),
+			"encrypted_session_key": encrypted_session_key.hex(),
+			"aad": aad.hex(),
+			"ciphertext": ciphertext.hex()
+		}
+
+		return encrypted_data
+
 	def _save_notes(self, notes):
 		"""Saves the current list of notes to the user's file."""
 		if notes:
-			session_key = generate_session_key()
-
-			notes_data = json.dumps(notes)
-
-			nonce, ciphertext, aad = encrypt_notes_data(notes_data, session_key, self.rsa_private_key)
-			encrypted_session_key = encrypt_session_key(self.rsa_public_key, session_key)
-
-			encrypted_data = {
-				"nonce": nonce.hex(),
-				"encrypted_session_key": encrypted_session_key.hex(),
-				"aad": aad.hex(),
-				"ciphertext": ciphertext.hex()
-			}
+			encrypted_data = self._encrypt_notes_file(notes)
 
 			with open(self.notes_file, 'w') as file:
 				json.dump(encrypted_data, file, indent=4)
+
 			self.printer.print_debug("[CRYPTO LOG] Notes saved and encrypted with ChaCha20Poly1305, key length: 256 bits.")
 		else:
 			if os.path.exists(self.notes_file):
